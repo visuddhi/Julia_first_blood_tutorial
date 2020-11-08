633187fede68ebd9b333dfacf882ae4f5e2aae5c
Hanzhang Qin <hqin@mit.edu>
2020-11-08 05:38:43 -0500

    Add files via upload

---
+++ FancyStructs.jl
@@ -0,0 +1,12 @@
+# 单独保存一个.jl文件（头文件），和刚才的notebook放在同一路纪念馆下
+module FancyStructs
+  struct Coordinate{T}
+    x::T
+    y::T
+    z::T
+  end
+  function CoordinateAdd(obj1::FancyStructs.Coordinate, obj2::FancyStructs.Coordinate)
+    println("(", obj1.x, ",", obj1.y, "," , obj1.z,")",  " + ", "(", obj2.x, ",", obj2.y, "," , obj2.z,")", 
+        " = ", "(",obj1.x + obj2.x, ",", obj1.y + obj2.y, "," , obj1.z + obj2.z,")"  )
+  end
+end
